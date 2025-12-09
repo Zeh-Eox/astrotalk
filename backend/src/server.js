7fae9ae8ab25c488be834bfc6293f7d43bae4d81
Zeh-Eox <arnolddevmail@gmail.com>
2025-12-09 00:07:55 +0000

    Signup method added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path"
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 import authRoutes from "./routes/auth.route.js"
 import messagesRoutes from "./routes/message.route.js"
 import {connectDatabase} from "./lib/database.js";
+import { ENV } from "./lib/env.js";
 
 dotenv.config();
 
@@ -14,6 +16,7 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}))
 app.use(express.json()); // req.body access in controllers
 app.use(cookieParser());
 
