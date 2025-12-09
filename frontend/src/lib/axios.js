b267d032595d1c4dbb270ef8ea7a88ab0c38755c
Zeh-Eox <arnolddevmail@gmail.com>
2025-12-09 00:06:01 +0000

    Signup method added

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const HTTP_CLIENT = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api/v1/" : "/api",
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,3 +1,35 @@
 import { create } from "zustand";
+import { HTTP_CLIENT } from "../lib/axios";
+import toast from "react-hot-toast";
 
-export const useAuthStore = create((set, get) => ({}))
+export const useAuthStore = create((set, get) => ({
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
+
+  checkAuth: async () => {
+    try {
+      const response = await HTTP_CLIENT.get("/auth/check");
+      set({authUser: response.data});
+    } catch (error) {
+      console.error("Error in authCheck: ", error);
+      set({authUser: null});
+    } finally {
+      set({isCheckingAuth: false})
+    }
+  },
+
+  signup: async (data) => {
+    set({isSigningUp: true})
+    try {
+      const response = await HTTP_CLIENT.post("/auth/signup", data);
+      set({authUser: response.data});
+
+      toast.success("Account Created Successfully")
+    } catch (error) {
+      toast.error(error.response.data.message)
+    } finally {
+      set({isSigningUp: false})
+    }
+  },
+}))
